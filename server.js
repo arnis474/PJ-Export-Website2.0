0d7ba40a4d1f146b688f04e34d030833f0f22221
arnis474 <apiekus20@gmail.com>
2024-10-14 23:20:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Serve static files directly from the root directory
+app.use(express.static(__dirname)); // This will serve all files from the root directory
+
 // Define a route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Ensure the correct file name here
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-app.use(express.static(path.join(__dirname, 'static')));
-
