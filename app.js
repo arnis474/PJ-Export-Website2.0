72a4372542b619d7c500acc38f41ec95b8c55478
arnis474 <apiekus20@gmail.com>
2024-10-14 23:33:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-cconst express = require('express');
+const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
