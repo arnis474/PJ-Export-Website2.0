4d4e526811f2f23a5cac096fe8835383c9b4e060
arnis474 <apiekus20@gmail.com>
2024-10-14 23:15:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'static')));
-
 // Define a route for the homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html')); // Ensure the correct file name here
