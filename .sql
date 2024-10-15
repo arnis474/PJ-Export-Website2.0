c07f570bf93737269feb969891eb580c7ff2598f
arnis474 <apiekus20@gmail.com>
2024-10-15 13:14:11 +0100

    names

---
+++ .sql
@@ -0,0 +1,20 @@
+AboutUs/
+   └── index.html  (from aboutus.html)
+
+UsedClothes/
+   └── index.html  (from usedclothes.html)
+
+Contact/
+   └── index.html  (from contact.html)
+
+Returns/
+   └── index.html  (from returns.html)
+
+Shipping/
+   └── index.html  (from shipping.html)
+
+WipingRags/
+   └── index.html  (from wipingrags.html)
+
+FAQ/
+   └── index.html  (from faq.html)
