09ae9e36965a96a60845e283d3e61d53d3ed2d2c
arnis474 <apiekus20@gmail.com>
2024-10-17 00:34:30 +0100

    aj

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const sgMail = require('@sendgrid/mail');
+const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const device = require('express-device');  // Import express-device
 
 const app = express();
 const port = 3000;
 
-// Middleware to serve static files (CSS, images, etc.) from the root directory
-app.use(express.static(__dirname));
+// Middleware to serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware to detect device
-app.use(device.capture());
-
-// Route to serve the correct homepage based on device type
+// Route to serve the index.html page
 app.get('/', (req, res) => {
-  if (req.device.type === 'phone' || req.device.type === 'tablet') {
-    // Serve the mobile version of the homepage
-    res.sendFile(path.join(__dirname, 'mobile.html'));
-  } else {
-    // Serve the desktop version of the homepage
-    res.sendFile(path.join(__dirname, 'home.html'));
-  }
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-// Set your SendGrid API key
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Route to handle form submission
 app.post('/submit-form', (req, res) => {
   const { name, email, message } = req.body;
 
+  // Set up nodemailer to send email
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'customermail532@gmail.com', // Replace with your email
+      pass: 'epzg xifa nwqs opuu',    // Replace with your app-specific password
+    },
+  });
+
   // Email options
-  const msg = {
-    to: 'info@pjexport.co.uk',
-    from: email,
-    subject: `New Message from ${name}`,
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    html: `<strong>Name: ${name}</strong><br><strong>Email:</strong> ${email}<br><strong>Message:</strong><br>${message}`,
+  let mailOptions = {
+    from: email,  
+    to: 'pjexport@yahoo.com',  // Replace with the recipient's email
+    subject: `New Message from ${name}`,  
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,  
   };
 
   // Send the email
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-      res.status(200).send('Message sent successfully!');
-    })
-    .catch((error) => {
-      console.error('Error sending email:', error.response.body);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
       res.status(500).send('There was an error sending the email.');
-    });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Message sent successfully!');
+    }
+  });
 });
 
 // Start the server
